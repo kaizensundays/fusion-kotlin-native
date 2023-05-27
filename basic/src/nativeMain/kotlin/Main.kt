3b5d92e67e4f769da916eacb3a209e0673251df3
KaizenSundays <kaizensundays@localhost>
2023-05-27 16:00:11 -0400

    [ fusion-kotlin-native ] basic

--- basic/src/nativeMain/kotlin/Main.kt
+++ basic/src/nativeMain/kotlin/Main.kt
@@ -1,14 +1,9 @@
 package com.kaizensundays.fusion.native
 
 fun main() {
-/*
-    println("Enter a string to reverse:")
 
-    val input = readln()
-
-    println(input.reversed())
-*/
     val map = mapOf("1" to "A", "3" to "B", "7" to "C")
 
     println("map=$map")
+
 }
