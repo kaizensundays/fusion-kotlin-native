a95228c67565a7b7869031b26fbf18246f4b41eb
KaizenSundays <kaizensundays@localhost>
2023-05-27 16:02:08 -0400

    [ fusion-kotlin-native ] basic

---
+++ basic/src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package com.kaizensundays.fusion.native
+
+fun main() {
+
+    val map = mapOf("1" to "A", "3" to "B", "7" to "C")
+
+    println("map=$map")
+
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+rootProject.name = "fusion-kotlin-native"
+
+include("basic")
+
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
